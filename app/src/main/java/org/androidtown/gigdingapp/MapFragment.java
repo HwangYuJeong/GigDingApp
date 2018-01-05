751ff716046659ad74bdc5c0cccddbff0a2605c7
DESKTOP-4LBBFU2\Kim <jin4332@gmail.com>
2018-01-05 15:13:12 +0900

    지도띄우기

--- app/src/main/java/org/androidtown/gigdingapp/MapFragment.java
+++ app/src/main/java/org/androidtown/gigdingapp/MapFragment.java
@@ -1,10 +1,13 @@
 package org.androidtown.gigdingapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 public class MapFragment extends Fragment {
 
@@ -16,6 +19,15 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        Context context = getContext();
+        MapView mapView = new MapView(context);
+
+        ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        return view;
     }
 }
